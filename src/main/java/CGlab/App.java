710d747882428d13825c19a9dd9a9ae966cc3842
Michał Wajdowicz <102074612+MichalWajdowicz@users.noreply.github.com>
2022-04-11 22:12:25 +0200

    Zadanie 4 lab2

--- src/main/java/CGlab/App.java
+++ src/main/java/CGlab/App.java
@@ -17,7 +17,14 @@ public class App {
 
         Renderer mainRenderer = new Renderer(path, w, h, l);
         mainRenderer.clear();
-        mainRenderer.drawLine(20,20,200,300);
+        mainRenderer.drawLine(320,240,480,480);//1
+        mainRenderer.drawLine(320,240,160,480);//2
+        mainRenderer.drawLine(320,240,0,360);//3
+        mainRenderer.drawLine(320,240,0,120);//4
+        mainRenderer.drawLine(320,240,160,0);//5
+        mainRenderer.drawLine(320,240,480,0);//6
+        mainRenderer.drawLine(320,240,639,120);//7
+        mainRenderer.drawLine(320,240,639,360);//8
 
         try {
             mainRenderer.save();

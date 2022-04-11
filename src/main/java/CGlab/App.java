d86df830238f74ee150fe00c9835329539037482
Michał Wajdowicz <102074612+MichalWajdowicz@users.noreply.github.com>
2022-04-11 22:03:25 +0200

    Zadanie 1 lab2

---
+++ src/main/java/CGlab/App.java
@@ -0,0 +1,30 @@
+package CGlab;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App {
+
+    String version = "0.02";
+
+    public static void main(String[] args) {
+
+        String path = args[0];
+        int w = Integer.parseInt(args[1]);
+        int h = Integer.parseInt(args[2]);
+
+        Renderer mainRenderer = new Renderer(path, w, h);
+        mainRenderer.clear();
+        mainRenderer.drawPoint(100, 100);
+        try {
+            mainRenderer.save();
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getVersion() {
+	return this.version;
+    }
+}
